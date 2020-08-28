7745e5cbd4d2f91a5643cd0e80435eec8d9eec49
jihoonys82 <jihoonys82@gmail.com>
2020-08-27 23:51:27 -0700

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import firebase from './src/firebase/firebase';
+//import firebase from './src/firebase/firebase';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>Hi Inklings!!!</Text>
         <StatusBar style="auto" />
       </View>
     </>
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
