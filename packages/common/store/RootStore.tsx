56cf2bba183542bc12d5775a26ba0dc70e6df4fe
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2020-08-28 15:56:39 -0700

    set mobx store

--- packages/common/store/RootStore.tsx
+++ packages/common/store/RootStore.tsx
@@ -1,20 +1,13 @@
-import { observable, reaction, computed, autorun, action, transaction } from 'mobx';
+import { observable, action } from 'mobx';
 
-const calculator = observable({
-    a: 1,
-    b: 2,
-});
-
-// **** 특정 값이 바뀔 때 특정 작업 하기!
-reaction(() => calculator.a, (value, reaction) => {
-      console.log(`a 값이 ${value} 로 바뀌었네요!`);
-    }
-  );
+export default class RootStore {
+    @observable number = 0;
   
-  reaction(() => calculator.b, value => {
-      console.log(`b 값이 ${value} 로 바뀌었네요!`);
-  }
-);
-
-calculator.a = 10;
-  calculator.b = 20;
+    @action increase = () => {
+      this.number++;
+    }
+  
+    @action decrease = () => {
+      this.number--;
+    }
+}
