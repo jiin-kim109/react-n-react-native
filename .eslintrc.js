06634a48a99f9e5185cf9249f8e04209dbc90355
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2020-08-30 01:59:45 -0700

    apply eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'prettier/react',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'react-hooks'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      'linebreak-style': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  };
