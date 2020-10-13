5d2ee56aa83e557aa74555d8d9a3d7c6b3fa98e0
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2020-10-13 11:58:44 -0700

    create master

--- packages/controllers/src/firebase/Firebase.ts
+++ packages/controllers/src/firebase/Firebase.ts
@@ -1,22 +1,12 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'
 import { store } from "../store/store"
+import config from "./config"
 
 /**
  *  Initialize Firebase
  */ 
-const firebaseConfig = {
-    apiKey: "AIzaSyCDq2VErMgE9PycNPHr0U6TZYIJ72653Sw",
-    authDomain: "act-dev-a4876.firebaseapp.com",
-    databaseURL: "https://act-dev-a4876.firebaseio.com",
-    projectId: "act-dev-a4876",
-    storageBucket: "act-dev-a4876.appspot.com",
-    messagingSenderId: "1016558907429",
-    appId: "1:1016558907429:web:08ded1a47dad15312bcfcc",
-    measurementId: "G-41Q5BHZBW8"
-};
-
 if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(config);
     firebase.auth().signOut(); // TEMPORARY: NEED TO DELETE WHEN Sign In function is fully implemented. 
 }
 
---
+++ packages/controllers/src/firebase/config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDq2VErMgE9PycNPHr0U6TZYIJ72653Sw",
+    authDomain: "act-dev-a4876.firebaseapp.com",
+    databaseURL: "https://act-dev-a4876.firebaseio.com",
+    projectId: "act-dev-a4876",
+    storageBucket: "act-dev-a4876.appspot.com",
+    messagingSenderId: "1016558907429",
+    appId: "1:1016558907429:web:08ded1a47dad15312bcfcc",
+    measurementId: "G-41Q5BHZBW8"
+};
+
+export default firebaseConfig;
